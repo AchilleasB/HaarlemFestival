553d401b793cc91a21da4407f5a9a66768ae1434
AchilleasB <90069683+AchilleasB@users.noreply.github.com>
2024-02-07 23:43:47 +0100

    created header with navigation created footer

---
+++ app/views/footer.php
@@ -0,0 +1,18 @@
+<footer>
+    <div class="p-3 text-center">
+        <div class="foot-nav">
+            <a href="/home/index">Home</a>
+            <a href="/history/index">History</a>
+            <a href="/cuisine/index">Cuisines</a>
+            <a href="/music/index">Music</a>
+            <a href="/festival/index">Festival</a>
+        </div>
+        <p>All rights reserved © - Designed and developed by ITGR5,Inc 2024</p>
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
