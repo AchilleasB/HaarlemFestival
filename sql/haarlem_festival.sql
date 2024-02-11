d646a2b64055a318539e2dbbbdacba1d2ee65980
AchilleasB <90069683+AchilleasB@users.noreply.github.com>
2024-02-11 19:26:05 +0100

    updated db

--- sql/haarlem_festival.sql
+++ sql/haarlem_festival.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Generation Time: Feb 10, 2024 at 10:18 AM
+-- Generation Time: Feb 11, 2024 at 06:25 PM
 -- Server version: 11.0.3-MariaDB-1:11.0.3+maria~ubu2204
 -- PHP Version: 8.2.9
 
@@ -24,16 +24,84 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `artists`
+--
+
+CREATE TABLE `artists` (
+  `id` int(11) NOT NULL,
+  `artist_name` varchar(50) NOT NULL,
+  `genre` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `id` int(11) NOT NULL,
+  `type` varchar(100) NOT NULL,
+  `date_time` varchar(50) NOT NULL,
+  `location_id` int(11) NOT NULL,
+  `artist_id` int(11) NOT NULL,
+  `tickets_available` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `locations`
+--
+
+CREATE TABLE `locations` (
+  `id` int(11) NOT NULL,
+  `location_name` varchar(100) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `location_type` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `date_time` date NOT NULL,
+  `payment_status` varchar(100) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tickets`
+--
+
+CREATE TABLE `tickets` (
+  `id` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `ticket_type` varchar(50) NOT NULL,
+  `ticket_amount` int(11) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `firstname` varchar(50) NOT NULL,
-  `lastname` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` varchar(50) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
   `registration_date` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -41,15 +109,49 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `role`, `registration_date`) VALUES
-(1, 'Achilleas', 'Ballanos', 'achil@email.com', '$2y$10$mjD.jC/87HN3E9q1ZxwXHeY6Yvh3WNLpLMrWlTny9iRGaoDXBde0a', 'customer', '07-02-2024'),
-(2, 'Odin', 'Odinson', 'odin@email.com', '$2y$10$9nQENvOLLdlj.RnMpB6ZpOnfTIyanK3msL/q/8Yu/NvtldzrIdXRe', 'customer', '08-02-2024'),
-(3, 'Tony', 'Stark', '686446@student.inholland.nl', '$2y$10$gikeOLWBalPi/..Wm8j.8.R1yRjtBplqytL9777xiXMflP5hBApb2', 'admin', '08-02-2024'),
-(4, 'Hulk', 'Banner', 'hulk@email.com', '$2y$10$6mVTfKfk.AdHFY4o2CRN0OvP2KDG..2RNWOWb2S0iiEC28lfwum/2', 'employee', '08-02-2024');
+INSERT INTO `users` (`id`, `email`, `password`, `role`, `firstname`, `lastname`, `registration_date`) VALUES
+(1, 'ahilleasballanos@gmail.com', '$2y$10$1ieCTLfmvygGHzj.6UEt5OqRD9F68SyimR2FK8ZRtGGHd.KLvhHQC', 'admin', 'Achil', 'Ballanos', '11-02-2024'),
+(2, 'hulk@email.com', '$2y$10$OHrfraa8gWbiEHuKN1LulOeSiLKea5WpVcq5uJZxivD0hefkWBAQy', 'employee', 'Hulk', 'Banner', '11-02-2024'),
+(3, 'thor@email.com', '$2y$10$9nNSxnov0RodE0oYBAHZ5eDW8q4DUMcJFPXVIWG3bqRsRSWV23CMC', 'customer', 'Thor', 'Odinson', '11-02-2024');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `artists`
+--
+ALTER TABLE `artists`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `artist` (`artist_id`),
+  ADD KEY `location` (`location_id`);
+
+--
+-- Indexes for table `locations`
+--
+ALTER TABLE `locations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user` (`user_id`);
+
+--
+-- Indexes for table `tickets`
+--
+ALTER TABLE `tickets`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `event` (`event_id`),
+  ADD KEY `order` (`order_id`);
 
 --
 -- Indexes for table `users`
@@ -62,10 +164,64 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `artists`
+--
+ALTER TABLE `artists`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `locations`
+--
+ALTER TABLE `locations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tickets`
+--
+ALTER TABLE `tickets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `events`
+--
+ALTER TABLE `events`
+  ADD CONSTRAINT `artist` FOREIGN KEY (`artist_id`) REFERENCES `artists` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `location` FOREIGN KEY (`location_id`) REFERENCES `locations` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tickets`
+--
+ALTER TABLE `tickets`
+  ADD CONSTRAINT `event` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `order` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
