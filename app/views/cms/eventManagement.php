8fa1671928acba5dbfa3177c375529dd3ccea21a
chabucca <89969146+chabucca@users.noreply.github.com>
2024-04-08 20:39:32 +0200

    event

--- app/views/cms/eventManagement.php
+++ app/views/cms/eventManagement.php
@@ -11,83 +11,128 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../styles/cms.css">
     <link rel="stylesheet" href="../styles/main.css">
-    <script src="https://cdn.tiny.cloud/1/dacel3kg9auup3593i648va8wcvi2j7ybudwbv0qmqbz74lc/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
+    <script src="https://cdn.tiny.cloud/1/dacel3kg9auup3593i648va8wcvi2j7ybudwbv0qmqbz74lc/tinymce/7/tinymce.min.js"
+        referrerpolicy="origin"></script>
 </head>
+
 <body>
 
     <?php
     include __DIR__ . '/../header.php';
     ?>
-<div class="p-5">
-        <ul class="nav nav-tabs justify-content-end" id="pageTabs" role="tablist">
-            <li class="nav-item" role="presentation">
-                <button class="nav-link active" id="addPageTab" data-bs-toggle="tab" data-bs-target="#addPage" type="button"
-                    role="tab" aria-controls="addPage" aria-selected="true">Add Page</button>
-            </li>
-            <li class="nav-item" role="presentation">
-                <button class="nav-link" id="editPageTab" data-bs-toggle="tab" data-bs-target="#editPage" type="button"
-                    role="tab" aria-controls="editPage" aria-selected="false">Edit Page</button>
-            </li>
-            <li class="nav-item" role="presentation">
-                <button class="nav-link" id="deletePageTab" data-bs-toggle="tab" data-bs-target="#deletePage" type="button"
-                    role="tab" aria-controls="deletePage" aria-selected="false">Delete Page</button>
-            </li>
-        </ul>
-        <div class="tab-content" id="pageTabsContent">
-            <div class="tab-pane fade show active" id="addPage" role="tabpanel" aria-labelledby="addPageTab">
+
+    <ul class="nav nav-tabs" id="eventPageTabs" role="tablist">
+        <li class="nav-item" role="presentation">
+            <button class="nav-link active" id="addEventPageTab" data-bs-toggle="tab" data-bs-target="#addEventPage"
+                type="button" role="tab" aria-controls="addEventPage" aria-selected="true">Add Event Page</button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="editEventPageTab" data-bs-toggle="tab" data-bs-target="#editEventPage"
+                type="button" role="tab" aria-controls="editEventPage" aria-selected="false">Edit Event Page</button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="deleteEventPageTab" data-bs-toggle="tab" data-bs-target="#deleteEventPage"
+                type="button" role="tab" aria-controls="deleteEventPage" aria-selected="false">Delete Event
+                Page</button>
+        </li>
+    </ul>
+
+    <div class="tab-content" id="eventPageTabContent">
+        <div class="tab-pane fade show active" id="addEventPage" role="tabpanel" aria-labelledby="addEventPageTab">
+            <div class="p-5">
                 <form action="/cms/createEventPage" method="post">
                     <label for="title">Title:</label>
-                    <input type="text" id="title" name="title" required><br><br>
+                    <textarea type="text" id="title" name="title"></textarea><br><br>
 
                     <label for="subtitle">Subtitle:</label>
-                    <input type="text" id="subtitle" name="subtitle"><br><br>
+                    <textarea type="text" id="subtitle" name="subtitle"></textarea><br><br>
 
                     <label for="description">Description:</label><br>
-                    <textarea id="description" name="description" class="tinymce-field" rows="4" cols="50"></textarea><br><br>
+                    <textarea id="description" name="description" class="tinymce-field" rows="4"
+                        cols="50"></textarea><br><br>
 
                     <label for="information">Information:</label><br>
-                    <textarea id="information" name="information" class="tinymce-field" rows="4" cols="50"></textarea><br><br>
+                    <textarea id="information" name="information" class="tinymce-field" rows="4"
+                        cols="50"></textarea><br><br>
 
-                    <input type="submit" value="Create Event Page">
+                    <input type="submit" value="Create Event Page" class="btn btn-primary">
                 </form>
-            </div>
-            <div class="tab-pane fade" id="editPage" role="tabpanel" aria-labelledby="editPageTab">
-            <div class="container mt-4">
-                <?php foreach ($eventPages as $eventPage): ?>
-                <div class="card mb-3">
-                    <div class="card-body">
-                        <form action="/cms/updateEventPage" method="post">
-                            <input type="hidden" name="event_id" value="<?= $eventPage['id'] ?>">
-                            <div class="mb-3">
-                                <label for="title<?= $eventPage['id'] ?>" class="form-label">Title</label>
-                                <textarea class="form-control tinymce-field" id="title<?= $eventPage['id'] ?>" name="title" rows="3"><?= $eventPage['title'] ?></textarea>
-                            </div>
-                            <div class="mb-3">
-                                <label for="subtitle<?= $eventPage['id'] ?>" class="form-label">Subtitle</label>
-                                <textarea class="form-control tinymce-field" id="subtitle<?= $eventPage['id'] ?>" name="subtitle" rows="3"><?= $eventPage['sub_title'] ?></textarea>
-                            </div>
-                            <div class="mb-3">
-                                <label for="description<?= $eventPage['id'] ?>" class="form-label">Description</label>
-                                <textarea class="form-control tinymce-field" id="description<?= $eventPage['id'] ?>" name="description" rows="3"><?= $eventPage['description'] ?></textarea>
-                            </div>
-                            <div class="mb-3">
-                                <label for="information<?= $eventPage['id'] ?>" class="form-label">Information</label>
-                                <textarea class="form-control tinymce-field" id="information<?= $eventPage['id'] ?>" name="information" rows="3"><?= $eventPage['information'] ?></textarea>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Save</button>
-                        </form>
-                    </div>
-                </div>
-                <?php endforeach; ?>
             </div>
         </div>
-            <div class="tab-pane fade" id="deletePage" role="tabpanel" aria-labelledby="deletePageTab">
-                <form action="/cms/deleteEventPage" method="post" class="page-form">
-                    <label for="deleteId">Page ID to Delete:</label>
+        <div class="tab-pane fade" id="editEventPage" role="tabpanel" aria-labelledby="editEventPageTab">
+            <div class="container mt-4">
+                <ul class="nav nav-tabs" id="eventPageTabsNested" role="tablist">
+                    <?php foreach ($eventPages as $eventPage) : ?>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="eventPage-tab-<?= $eventPage['id'] ?>" data-bs-toggle="tab"
+                            data-bs-target="#eventPage-<?= $eventPage['id'] ?>" type="button" role="tab"
+                            aria-controls="eventPage-<?= $eventPage['id'] ?>"
+                            aria-selected="false"><?= $eventPage['title'] ?></button>
+                    </li>
+                    <?php endforeach; ?>
+                </ul>
+                <div class="tab-content" id="eventPageTabContentNested">
+                    <?php foreach ($eventPages as $eventPage) : ?>
+                    <div class="tab-pane fade" id="eventPage-<?= $eventPage['id'] ?>" role="tabpanel"
+                        aria-labelledby="eventPage-tab-<?= $eventPage['id'] ?>">
+                        <div class="p-5">
+                            <h3>Edit Event Page: <?= $eventPage['title'] ?></h3>
+                            <form action="/cms/updateEventPage" method="post">
+                                <input type="hidden" name="event_id" value="<?= $eventPage['id'] ?>">
+                                <div class="mb-3">
+                                    <label for="title<?= $eventPage['id'] ?>" class="form-label">Title</label>
+                                    <textarea class="form-control tinymce-field" id="title<?= $eventPage['id'] ?>"
+                                        name="title" rows="3"><?= $eventPage['title'] ?></textarea>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="subtitle<?= $eventPage['id'] ?>" class="form-label">Subtitle</label>
+                                    <textarea class="form-control tinymce-field" id="subtitle<?= $eventPage['id'] ?>"
+                                        name="subtitle" rows="3"><?= $eventPage['sub_title'] ?></textarea>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="description<?= $eventPage['id'] ?>"
+                                        class="form-label">Description</label>
+                                    <textarea class="form-control tinymce-field" id="description<?= $eventPage['id'] ?>"
+                                        name="description" rows="3"><?= $eventPage['description'] ?></textarea>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="information<?= $eventPage['id'] ?>"
+                                        class="form-label">Information</label>
+                                    <textarea class="form-control tinymce-field" id="information<?= $eventPage['id'] ?>"
+                                        name="information" rows="3"><?= $eventPage['information'] ?></textarea>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Save</button>
+                            </form>
+                        </div>
+                    </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        </div>
+        <div class="tab-pane fade" id="deleteEventPage" role="tabpanel" aria-labelledby="deleteEventPageTab">
+            <div class="p-5">
+                <form action="/cms/deleteEventPage" method="post">
+                    <label for="deleteId">Enter ID to Delete:</label>
                     <input type="text" id="deleteId" name="id" required><br><br>
 
-                    <input type="submit" value="Delete Page">
+                    <input type="submit" value="Delete Event Page" class= "btn btn-primary">
                 </form>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Title</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($eventPages as $eventPage) : ?>
+                        <tr>
+                            <td><?= $eventPage['id'] ?></td>
+                            <td><?= $eventPage['title'] ?></td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
@@ -96,17 +141,17 @@
     <script>
     function initializeTinyMCE() {
         tinymce.init({
-            selector: '.tinymce-field',
+            selector: 'textarea',
             plugins: 'autolink lists link',
             toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent | link',
             height: 300
         });
     }
     window.addEventListener('DOMContentLoaded', initializeTinyMCE);
-</script>
+    </script>
+
 
     <?php include __DIR__ . '/../footer.php'; ?>
 
-   
-</body>
 
+</body>
--- app/views/customPage/cch.php
+++
@@ -1,9 +0,0 @@
-<?php
-$pageTitle = 'cch';
-require_once(__DIR__ . '/../../views/header.php');
-?>
-<h1><?php echo $pageTitle; ?></h1>
-<h2>w</h2>
-<p></p>
-<p></p>
-<?php require_once(__DIR__ . '/../../views/footer.php'); ?>
